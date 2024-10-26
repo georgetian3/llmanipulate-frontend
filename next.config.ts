b3a08753fb34843f15c47e7e599e7af29631ef04
George Tian <me@georgetian.com>
2024-10-26 18:33:03 +1100

    init

---
+++ next.config.ts
@@ -0,0 +1,10 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  i18n: {
+    locales: ['en', 'zh'], // Add your supported locales
+    defaultLocale: 'en', // Set the default locale
+  },
+};
+
+export default nextConfig;
