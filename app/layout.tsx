f7c3ce0fc1399e9a3e89402ead4efaebe96d4344
George Tian <me@georgetian.com>
2025-01-21 00:03:19 +1100

    redux init

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,39 +1,85 @@
-import type { Metadata } from "next";
-import localFont from "next/font/local";
-import "../styles/globals.css";
-import { StateProvider } from "./context/StateContext"; // Import StateProvider
+import Image from "next/image";
+import type { ReactNode } from "react";
+import { StoreProvider } from "./StoreProvider";
+import { Nav } from "./components/Nav";
 
-// Load custom fonts
-const geistSans = localFont({
-    src: "./fonts/GeistVF.woff",
-    variable: "--font-geist-sans",
-    weight: "100 900",
-});
+import "./styles/globals.css";
+import styles from "./styles/layout.module.css";
 
-const geistMono = localFont({
-    src: "./fonts/GeistMonoVF.woff",
-    variable: "--font-geist-mono",
-    weight: "100 900",
-});
+interface Props {
+  readonly children: ReactNode;
+}
 
-// Define site metadata
-export const metadata: Metadata = {
-    title: "LLManipulate",
-};
+export default function RootLayout({ children }: Props) {
+  return (
+    <StoreProvider>
+      <html lang="en">
+        <body>
+          <section className={styles.container}>
+            <Nav />
 
-export default function RootLayout({
-                                       children,
-                                   }: Readonly<{
-    children: React.ReactNode;
-}>) {
-    return (
-        <html>
-        <body
-            className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        >
-        {/* Wrap application with the global state provider */}
-        <StateProvider>{children}</StateProvider>
+            <header className={styles.header}>
+              <Image
+                src="/logo.svg"
+                className={styles.logo}
+                alt="logo"
+                width={100}
+                height={100}
+              />
+            </header>
+
+            <main className={styles.main}>{children}</main>
+
+            <footer className={styles.footer}>
+              <span>Learn </span>
+              <a
+                className={styles.link}
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                React
+              </a>
+              <span>, </span>
+              <a
+                className={styles.link}
+                href="https://redux.js.org"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Redux
+              </a>
+              <span>, </span>
+              <a
+                className={styles.link}
+                href="https://redux-toolkit.js.org"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Redux Toolkit
+              </a>
+              <span>, </span>
+              <a
+                className={styles.link}
+                href="https://react-redux.js.org"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                React Redux
+              </a>
+              ,<span> and </span>
+              <a
+                className={styles.link}
+                href="https://reselect.js.org"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Reselect
+              </a>
+            </footer>
+          </section>
         </body>
-        </html>
-    );
+      </html>
+    </StoreProvider>
+  );
 }
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,87 +1,86 @@
 "use client";
-import {useState} from "react";
+import { useState } from "react";
 import { useRouter } from "next/navigation";
-import "../../styles/login_page.css";
-import { apiRequest } from "../utils";
-import { useStateContext } from "../context/StateContext";
+import { apiRequest } from "../../old/app/utils";
+// import { useStateContext } from "../../old/app/context/StateContext";
 
 
 export default function LoginPage() {
-    const [usercode, setUsercode] = useState("");
-    const [loading, setLoading] = useState(false);
-    const router = useRouter();
-    const { setState } = useStateContext();
+  const [usercode, setUsercode] = useState("");
+  const [loading, setLoading] = useState(false);
+  const router = useRouter();
+  // const { setState } = useStateContext();
 
-    async function fetchUserData(userId: string) {
-        try {
-            const response = await apiRequest(`/users/${userId}`, "GET")
-            if (response.ok) {
-                return await response.json();
-            } else if (response.status === 404) {
-                return null;
-            } else {
-                throw new Error(`Unexpected server error: ${response.status}`);
-            }
-        } catch (error) {
-            console.error("Error fetching user data:", error);
-            throw error;
-        }
+  async function fetchUserData(userId: string) {
+    try {
+      const response = await apiRequest(`/users/${userId}`, "GET")
+      if (response.ok) {
+        return await response.json();
+      } else if (response.status === 404) {
+        return null;
+      } else {
+        throw new Error(`Unexpected server error: ${response.status}`);
+      }
+    } catch (error) {
+      console.error("Error fetching user data:", error);
+      throw error;
+    }
+  }
+
+  const handleLogin = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (!usercode.trim()) {
+      alert("Please enter a Usercode.");
+      return;
     }
 
-    const handleLogin = async (e: React.FormEvent) => {
-        e.preventDefault();
+    setLoading(true);
+    try {
+      const userData = await fetchUserData(usercode.trim());
+      if (!userData) {
+        alert("No matching User found. Please try again.");
+        return;
+      }
 
-        if (!usercode.trim()) {
-            alert("Please enter a Usercode.");
-            return;
-        }
+      const state = {
+        taskType: userData.task_type,
+        taskId: "",
+        userId: usercode.trim(),
+        name: userData.demographics.name,
+        initialScores: { A: 1, B: 1, C: 1, D: 1, confidence: 1, familiarity: 1 },
+        options: [],
+      };
 
-        setLoading(true);
-        try {
-            const userData = await fetchUserData(usercode.trim());
-            if (!userData) {
-                alert("No matching User found. Please try again.");
-                return;
-            }
-
-            const state = {
-                taskType: userData.task_type,
-                taskId: "",
-                userId: usercode.trim(),
-                name: userData.demographics.name,
-                initialScores: { A:1,B:1,C:1,D:1, confidence: 1, familiarity: 1 },
-                options: [],
-            };
-
-            if (localStorage.getItem("state"))
-                localStorage.removeItem("state");
-            setState(state);
+      if (localStorage.getItem("state"))
+        localStorage.removeItem("state");
+      // setState(state);
 
 
-            router.push(`/tasks?`);
-        } catch (error) {
-            console.error("Error during login:", error);
-            alert("An unexpected error occurred. Please try again.");
-        } finally {
-            setLoading(false);
-        }
-    };
+      router.push(`/tasks?`);
+    } catch (error) {
+      console.error("Error during login:", error);
+      alert("An unexpected error occurred. Please try again.");
+    } finally {
+      setLoading(false);
+    }
+  };
 
-    return (
-        <div className="login-container">
-            <h1>Please enter your passcode below</h1>
-            <form onSubmit={handleLogin}>
-                <input
-                    type="text"
-                    // placeholder="Enter Usercode"
-                    value={usercode}
-                    onChange={(e) => setUsercode(e.target.value)}
-                    className="login-input"
-                />
-                <button type="submit" className="login-button" disabled={loading}>
-                    {loading ? "Loading..." : "Submit"}
-                </button>
-            </form>
-        </div>
-    );
+  return (
+    <div className="login-container">
+      <h1 className="text-red-500">Please enter your passcode below</h1>
+      <form onSubmit={handleLogin}>
+        <input
+          type="text"
+          // placeholder="Enter Usercode"
+          value={usercode}
+          onChange={(e) => setUsercode(e.target.value)}
+          className="login-input"
+        />
+        <button type="submit" className="login-button" disabled={loading}>
+          {loading ? "Loading..." : "Submit"}
+        </button>
+      </form>
+    </div>
+  );
 }
