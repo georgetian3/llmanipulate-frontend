be4fff38f582d19676b9b1ccc2e6db92fc37fa36
George Tian <me@georgetian.com>
2025-03-26 00:10:24 +1100

    update

--- lib/apis.ts
+++ lib/apis.ts
@@ -2,7 +2,7 @@ import { createTask, createTaskResponse, deleteTask, getMe, getMyTasks, getTask,
 import { createClient } from "@hey-api/client-fetch";
 import { ComponentResponsesType } from "./appSlice";
 
-const API_URL = process.env["NEXT_PUBLIC_API_URL"]
+const API_URL = process.env.NEXT_PUBLIC_API_URL
 console.log("API URL", API_URL)
 const client = createClient({ baseUrl: API_URL })
 
