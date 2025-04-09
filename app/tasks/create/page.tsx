368cc0db681f948a1758335075a2fb1dfa3c620d
George Tian <me@georgetian.com>
2025-04-10 00:31:38 +1000

    update

--- app/tasks/create/page.tsx
+++ app/tasks/create/page.tsx
@@ -1,12 +1,13 @@
 "use client"
 
+import { TaskConfigOutput } from "@/api";
 import { AuthGuard } from "@/components/auth";
 import api from "@/lib/apis";
-import { Button, Card, CardBody } from "@heroui/react";
+import { addToast, Button, Card, CardBody, Tab, Tabs } from "@heroui/react";
 import { Editor } from "@monaco-editor/react";
 import { useTheme } from "next-themes";
 import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { Dispatch, SetStateAction, useMemo, useState } from "react";
 
 
 
@@ -64,12 +65,24 @@ const defaultConfig = `{
 }
 `
 
-function TaskCreatePage() {
+
+
+function PreviewTab() {
+  return (
+    <div></div>
+  )
+
+}
+
+
+interface ConfigTabProps {
+  taskConfig: string
+  setTaskConfig: Dispatch<SetStateAction<string>>
+}
+
+function ConfigTab({ taskConfig, setTaskConfig }: ConfigTabProps) {
   const { theme } = useTheme();
   const [warning, setWarning] = useState("")
-  const [config, setConfig] = useState(defaultConfig)
-  const [creating, setCreating] = useState(false)
-  const router = useRouter()
 
 
   function handleChange(value: string | undefined) {
@@ -78,21 +91,57 @@ function TaskCreatePage() {
     }
     try {
       JSON.parse(value)
-      setConfig(value)
+      setTaskConfig(value)
       setWarning("")
     } catch {
       setWarning("Invalid JSON")
     }
   }
 
+
+  return (
+    <div className="flex flex-col w-full h-full justify-center items-center">
+      <Editor
+        className="p-4"
+        theme={theme === "light" ? "light" : "vs-dark"}
+        defaultLanguage="json"
+        defaultValue={taskConfig}
+        onChange={(value) => handleChange(value)}
+      />
+      {warning && <div className="text-danger">
+        {warning}
+      </div>}
+
+    </div>
+  )
+}
+
+function CreateTab() {
+  return (
+    <div></div>
+  )
+}
+
+
+function TaskCreatePage() {
+  const [taskConfig, setTaskConfig] = useState(defaultConfig)
+
+  const [creating, setCreating] = useState(false)
+  const router = useRouter()
+
+
   async function handleCreate() {
     setCreating(true)
     try {
-      const resp = await api.createTask({config: JSON.parse(config)})
+      const resp = await api.createTask({ config: JSON.parse(taskConfig) })
       if (resp.response.status < 300) {
         router.push("/tasks")
       } else {
-        setWarning(JSON.stringify(resp.error?.detail) ?? "Unknown error")
+        addToast({
+          title: "Error creating task, please try again",
+          description: JSON.stringify(resp.error?.detail),
+          color: "danger",
+        })
       }
     } catch (e) {
       console.log("Error creating task:", e)
@@ -101,36 +150,38 @@ function TaskCreatePage() {
   }
 
   return (
-    <div className="flex flex-col w-full h-full justify-center items-center p-4 gap-4">
-      <Card className="w-full h-full">
-        <CardBody>
-          <Editor
-            theme={theme === "light" ? "light" : "vs-dark"}
-            defaultLanguage="json"
-            defaultValue={defaultConfig}
-            onChange={(value) => handleChange(value)}
-          />
-        </CardBody>
-      </Card>
-      {warning && <div className="text-danger">
-        {warning}
-      </div>}
-      <Button
-        color="primary"
-        className="w-min"
-        isDisabled={!!warning}
-        isLoading={creating}
-        onPress={handleCreate}
-      >
-        Create
-      </Button>
+    <div className="h-[calc(100vh-120px)]">
+      <Tabs variant="underlined">
+        <Tab title="Config" className="w-full h-full">
+          <ConfigTab taskConfig={taskConfig} setTaskConfig={setTaskConfig} />
+        </Tab>
+        <Tab title="Create">
+          <CreateTab />
+        </Tab>
+        <Tab title="Preview">
+          <PreviewTab />
+        </Tab>
+      </Tabs>
+      {/* <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+        <Button
+          color="primary"
+          className="w-min"
+          isDisabled={!validConfig}
+          isLoading={creating}
+          onPress={handleCreate}
+        >
+          Create
+        </Button>
+      </div> */}
+
     </div>
   )
+
 }
 
 export default function AuthedTaskCreatePage() {
   return (
-    <AuthGuard admin>
+    <AuthGuard>
       <TaskCreatePage />
     </AuthGuard>
   )
